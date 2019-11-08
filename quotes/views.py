5f70af1e457d263d50c1b2af1d29c83cb9713d97
Jyothis Thomas <jyothisth@gmail.com>
2019-11-08 11:03:50 +0530

    removed sendgrid from requirements.txt

--- quotes/views.py
+++ quotes/views.py
@@ -208,42 +208,41 @@ def graph(request, ticker):
     import datetime
     from bokeh.plotting import figure, save, output_file
     #Code to obtain trade data for AAPL
-    API_KEY = ' V81XUIFVLP1JZ9KB'
+    API_KEY = 'V81XUIFVLP1JZ9KB'
     stock_name = ticker
-    r = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock_name + '&apikey=' + API_KEY)
-    print(r.status_code)
-    result = r.json()
+    json_stock_price = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + stock_name + '&apikey=' + API_KEY)
+    result = json_stock_price.json()
     dataForAllDays = result['Time Series (Daily)']
     #convert to dataframe
-    df = pd.DataFrame.from_dict(dataForAllDays, orient='index') 
-    df = df.reset_index()
+    dataForAllDays_df = pd.DataFrame.from_dict(dataForAllDays, orient='index') 
+    dataForAllDays_df = dataForAllDays_df.reset_index()
     #rename columns
-    df = df.rename(index=str, columns={"index": "date", "1. open": "open", "2. high": "high", "3. low": "low", "4. close": "close","5. volume":"volume"})
+    dataForAllDays_df = dataForAllDays_df.rename(index=str, columns={"index": "date", "1. open": "open", "2. high": "high", "3. low": "low", "4. close": "close","5. volume":"volume"})
     #Changing to datetime
-    df['date'] = pd.to_datetime(df['date'])
+    dataForAllDays_df['date'] = pd.to_datetime(dataForAllDays_df['date'])
     #Sort according to date
-    df = df.sort_values(by=['date'])
+    dataForAllDays_df = dataForAllDays_df.sort_values(by=['date'])
     #Changing the datatype 
-    df.open = df.open.astype(float)
-    df.close = df.close.astype(float)
-    df.high = df.high.astype(float)
-    df.low = df.low.astype(float)
-    df.volume = df.volume.astype(int)
+    dataForAllDays_df.open = dataForAllDays_df.open.astype(float)
+    dataForAllDays_df.close = dataForAllDays_df.close.astype(float)
+    dataForAllDays_df.high = dataForAllDays_df.high.astype(float)
+    dataForAllDays_df.low = dataForAllDays_df.low.astype(float)
+    dataForAllDays_df.volume = dataForAllDays_df.volume.astype(int)
     #check the data
-    df.head()
+    dataForAllDays_df.head()
     #Check the datatype
-    df.info()
-    inc = df.close > df.open
-    dec = df.open > df.close
+    dataForAllDays_df.info()
+    inc = dataForAllDays_df.close > dataForAllDays_df.open
+    dec = dataForAllDays_df.open > dataForAllDays_df.close
     w = 12*60*60*1000 # half day in ms
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
     title = stock_name + ' Chart'
     p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = title, x_axis_label= "Date", y_axis_label= "Stock Price")
     p.xaxis.major_label_orientation = math.pi/4
     p.grid.grid_line_alpha=0.3
-    p.segment(df.date, df.high, df.date, df.low, color="black")
-    p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
-    p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+    p.segment(dataForAllDays_df.date, dataForAllDays_df.high, dataForAllDays_df.date, dataForAllDays_df.low, color="black")
+    p.vbar(dataForAllDays_df.date[inc], w, dataForAllDays_df.open[inc], dataForAllDays_df.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(dataForAllDays_df.date[dec], w, dataForAllDays_df.open[dec], dataForAllDays_df.close[dec], fill_color="#F2583E", line_color="black")
     #Store as a HTML file
     output_file("quotes/templates/stock_information.html", title="candlestick.py example")
     save(p)
